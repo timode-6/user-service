9cdf04d6e7fddb42530a2d7dac209c18d2dcdbd8
timode-6 <aleckeychick.tim@gmail.com>
2026-03-08 21:59:25 +0300

    init commit

---
+++ src/test/java/com/example/user_service/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.user_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
